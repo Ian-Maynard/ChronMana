500b397a73be7a950d622ce575515634f101c446
Ian-Maynard <maynard.ian@gmail.com>
2020-03-06 21:51:39 -0500

    app and server

---
+++ controller/user.controller.js
@@ -0,0 +1,4 @@
+/* jshint esversion: 6 */
+/* jshint esversion: 8 */
+import express from 'express';
+ 
---
+++ server/app.js
@@ -0,0 +1,19 @@
+/* jshint esversion: 6 */
+/* jshint esversion: 8 */
+// eslint-disable-next-line linebreak-style
+
+// Import all dependencies & middleware here
+import express from 'express';
+// Init an Express App.
+const app = express();
+// Use your dependencies here
+// use all controllers(APIs) here
+app.get('/', (req, res) => {
+  // eslint-disable-next-line no-trailing-spaces
+  res.status(200).json({ status: 'Server Run successfully' }); 
+});
+// Start Server here
+app.listen(8080, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server is running on port 8080!');
+});
